d08c3f490c552edd1bfd25792517140e772f753f
1u0Hun <1105364442@qq.com>
2024-03-29 00:57:49 +0800

    修改my页面，将webdav、关于等页面进行修改

--- app/src/main/java/io/legado/app/ui/main/MainActivity.kt
+++ app/src/main/java/io/legado/app/ui/main/MainActivity.kt
@@ -43,11 +43,16 @@ import io.legado.app.ui.main.explore.ExploreFragment
 import io.legado.app.ui.main.my.MyFragment
 import io.legado.app.ui.main.rss.RssFragment
 import io.legado.app.ui.widget.dialog.TextDialog
-import io.legado.app.utils.*
+import io.legado.app.utils.hideSoftInput
+import io.legado.app.utils.observeEvent
+import io.legado.app.utils.setEdgeEffectColor
+import io.legado.app.utils.showDialogFragment
+import io.legado.app.utils.toastOnUi
 import io.legado.app.utils.viewbindingdelegate.viewBinding
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.collections.set
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
